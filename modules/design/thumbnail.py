6ac7bbf0118d2da8a2cbc1b25b4d73a17780775d
Aditya Halder <95334522+MrAdityaHalder@users.noreply.github.com>
2022-02-07 22:50:04 +0530

    Update thumbnail.py

--- modules/design/thumbnail.py
+++ modules/design/thumbnail.py
@@ -17,7 +17,7 @@ async def thumb(thumbnail, title, userid):
     async with aiohttp.ClientSession() as session:
         async with session.get(thumbnail) as resp:
             if resp.status == 200:
-                f = await aiofiles.open(f"search/thumb{userid}.png", mode="wb")
+                f = await aiofiles.open(f"resource/thumb{userid}.png", mode="wb")
                 await f.write(await resp.read())
                 await f.close()
     image1 = Image.open(f"resource/thumb{userid}.png")
